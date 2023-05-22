0ba5dbbd0b2123841462da3d29f83cebf5563b18
Santiago Sánchez <sasanchezramirez@gmail.com>
2023-05-22 11:58:22 -0500

    Domain layer is done

--- src/main/java/com/hexagonal/tasks/domain/ports/out/ExternalServicePort.java
+++ src/main/java/com/hexagonal/tasks/domain/ports/out/ExternalServicePort.java
@@ -1,2 +1,7 @@
-package com.hexagonal.tasks.domain.ports.out;public interface ExternalServicePort {
+package com.hexagonal.tasks.domain.ports.out;
+
+import com.hexagonal.tasks.domain.models.AdditionalTasksInfo;
+
+public interface ExternalServicePort {
+    AdditionalTasksInfo getAdditionalTaskInfo(Long taskId);
 }
--- src/main/java/com/hexagonal/tasks/domain/ports/out/TaskRepositoryPort.java
+++ src/main/java/com/hexagonal/tasks/domain/ports/out/TaskRepositoryPort.java
@@ -1,2 +1,14 @@
-package com.hexagonal.tasks.domain.ports.out;public interface TaskRepositoryPort {
+package com.hexagonal.tasks.domain.ports.out;
+
+import com.hexagonal.tasks.domain.models.Task;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface TaskRepositoryPort {
+    Task save(Long id);
+    Optional<Task> findById(Long id);
+    List<Task> findAll();
+    Optional<Task> update(Task task);
+    Boolean deleteById(Long id);
 }
